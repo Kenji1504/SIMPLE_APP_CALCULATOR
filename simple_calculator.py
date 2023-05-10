5e627ff7132b22b974c18f50873fc9de9fb8d064
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-10 10:01:01 +0800

    Pseudo code

---
+++ simple_calculator.py
@@ -0,0 +1,30 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# This program will calculate the two inputted numbers entered
+# by the user using the basic mathematical operations.
+# Appropriate exception capturing codes will be also implemented in this program.
+
+# create a calculator function
+    # create a while loop
+	    # try asking the user to input two numerical values.
+            # break the loop
+	    # use except function to capture any Value Error
+	# ask user for what operator to use
+	# if response is '+', add two values
+	# if response '-', subtract the first inputted number to the second inputted number
+	# if response is 'x', multiply the first inputted number to the second inputted number
+	# if response is '/' 
+        # try dividing the first inputted number to the second inputted number
+        # use except function to capture any Zero Division Error
+        # break the loop
+	# else, print Invalid Input
+
+# To loop the program, create a while loop
+	# call out the calculator function
+	# ask user if they want to try again
+		# if response is 'yes', continue
+		# if response in 'no', break
+		# else 
+			# print 'Invalid Input'
+			# ask user to try again	
+# end program
