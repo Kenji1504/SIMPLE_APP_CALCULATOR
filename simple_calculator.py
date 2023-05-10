8db78d15f75ead601ff7516f5fa81fa3689d1b1d
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-10 12:47:24 +0800

    to loop the program

--- simple_calculator.py
+++ simple_calculator.py
@@ -16,85 +16,106 @@ time.sleep(1)
 print("\033[1m" + "\nWelcome to SIMPLE CALCULATOR.") 
 time.sleep(1)
 print("\nPlease input the following requirements to calculate for the resulting value.\n")
-time.sleep(1)
+
 # create a calculator function
 def calculator():
     # create a while loop
     while True:
 	    # try asking the user to input two numerical values.
         try:
+            time.sleep(1)
             first_value = float(input("Input the first value: "))
+            time.sleep(1)
             second_value = float(input("\nInput the second value: "))
             # break the loop if inputs are valid
             break
 	    # use except function to capture any Value Error
         except ValueError:
-            print("\nInvalid Input, input numerical characters only.\n")
-            print("Please try again.\n")
+            print("\033[31m" + "\nInvalid Input, input numerical characters only.\n")
+            print("\033[95m" + "Please try again.\n")
             time.sleep(1)
 	
-    # ask user for what operator to use
-    print("\nInput one of the following values to determine what mathematical operator will be used for calculation.\n")
-    time.sleep(1)
-    print("Use '+' to indicate addition\nUse '-' to indicate subtraction\
-          \nUse 'x' to indicate multiplication\nUse'/' to indicate division")
-    time.sleep(1)
-    math_operator = input("\nInput what operator to use: ")
-    math_operator = math_operator.lower()
-    time.sleep(1)
-    for dot in range(3):
-        print(".\n")
+    while True:
+        # ask user for what operator to use
+        print("\033[95m" + "\nInput one of the following values to determine what mathematical operator will be used for calculation.\n")
         time.sleep(1)
+        print("Use '+' to indicate addition\nUse '-' to indicate subtraction\
+            \nUse 'x' to indicate multiplication\nUse'/' to indicate division")
+        time.sleep(1)
+        math_operator = str(input("\nInput what operator to use: "))
+        math_operator = math_operator.lower()
+        time.sleep(1)
+        
+        for dot in range(3):
+            print(".\n")
+            time.sleep(1)
     
-	# if response is '+', add two values
-    if math_operator == '+':
-        print("Here's for the answer:")
-        resultant_value = first_value + second_value
-        time.sleep(1)
-        sum = f"\n{first_value} + {second_value} = {resultant_value}"
-        print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(sum.center(65), font= "ntgreek"))
-
-	# if response '-', subtract the first inputted number to the second inputted number
-    elif math_operator == '-':
-        print("Here's for the answer:")
-        resultant_value = first_value - second_value
-        time.sleep(1)
-        difference = f"\n{first_value} - {second_value} = {resultant_value}"
-        print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(difference.center(65), font= "ntgreek")) 
-
-	# if response is 'x', multiply the first inputted number to the second inputted number
-    elif math_operator == 'x':
-        print("Here's for the answer:")
-        resultant_value = first_value * second_value
-        time.sleep(1)
-        product = f"\n{first_value} x {second_value} = {resultant_value}"
-        print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(product.center(65), font= "ogre"))
-	# if response is '/' 
-    elif math_operator == '/':
-        # inside while loop
-        while True:
-            # try dividing the first inputted number to the second inputted number
-            try:
-                resultant_value = first_value / second_value
-                quotient = f"\n{first_value} / {second_value} = {resultant_value}"
-                print("Here's for the answer:")
-                print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(quotient.center(65), font= "ogre"))
-            # use except function to capture any Zero Division Error
-            except ZeroDivisionError:
-                print("\nInvalid Input. You're trying to divide a particular number by 0\n")
-                print("Please try again.")
-            # break the loop
+        # if response is '+', add two values
+        if math_operator == '+':
+            print("Here's for the answer:")
+            resultant_value = first_value + second_value
+            time.sleep(1)
+            sum = f"\n{first_value} + {second_value} = {resultant_value}"
+            print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(sum.center(65), font= "ntgreek"))
             break
-	# else, print Invalid Input
+
+        # if response '-', subtract the first inputted number to the second inputted number
+        elif math_operator == '-':
+            print("Here's for the answer:")
+            resultant_value = first_value - second_value
+            time.sleep(1)
+            difference = f"\n{first_value} - {second_value} = {resultant_value}"
+            print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(difference.center(65), font= "ntgreek"))
+            break 
+
+        # if response is 'x', multiply the first inputted number to the second inputted number
+        elif math_operator == 'x':
+            print("Here's for the answer:")
+            resultant_value = first_value * second_value
+            time.sleep(1)
+            product = f"\n{first_value} x {second_value} = {resultant_value}"
+            print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(product.center(65), font= "ogre"))
+            break
+        # if response is '/' 
+        elif math_operator == '/':
+            # inside while loop
+            while True:
+                # try dividing the first inputted number to the second inputted number
+                try:
+                    resultant_value = first_value / second_value
+                    quotient = f"\n{first_value} / {second_value} = {resultant_value}"
+                    print("Here's for the answer:")
+                    print("\033[1m" + "\033[95m" + pyfiglet.figlet_format(quotient.center(65), font= "ogre"))
+                    break
+                # use except function to capture any Zero Division Error
+                except ZeroDivisionError:
+                    print("\033[31m" + "\nInvalid Input. You're trying to divide a particular number by 0\n")
+                    print("\033[95m" + "Please try again.")
+                    time.sleep(1)
+                # break the loop
+                break
+            break
+        # else, print Invalid Input
+        else:
+            print("\033[31m" + "INVALID INPUT, please try again")
 
 # To loop the program, create a while loop
-	# call out the calculator function
-	# ask user if they want to try again
-		# if response is 'yes', continue
-		# if response in 'no', break
-		# else 
-			# print 'Invalid Input'
-			# ask user to try again	
-# end program
+while True:
+    # call out the calculator function
+    calculator()
+    # ask user if they want to try again
+    time.sleep(1)
+    while True:
+        repeat_program = str(input("\033[95m" + "\nDo you want to try again? (y/n): "))
+        repeat_program = repeat_program.lower()
+        # if response is 'yes', continue
+        if repeat_program == "y":
+            continue
+        # if response in 'no', break
+        elif repeat_program == "n":
+            exit()
+        else:
+            # print 'Invalid Input'
+            print("\n\033[31m" + "INVALID INPUT, please try again.")
 
-calculator()
+# end program
